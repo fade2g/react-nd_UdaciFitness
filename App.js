977b9af41de054a34722785db2481fc85f29d267
fade2g <holger.heymanns@gmail.com>
2017-09-17 21:34:25 +0200

    feature: Added some JavaScript code for further development

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#Fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
