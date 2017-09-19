30cd98fcbe6c4f19d75678107b538ea5eb203c27
fade2g <holger.heymanns@gmail.com>
2017-09-19 20:24:48 +0200

    feature: Add value modification methods for change handlers

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,47 @@ import {View} from 'react-native';
 import {getMetricsMetaInfo} from "../utils/helpers";
 
 export default class AddEntry extends Component {
+
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0
+  };
+
+  increment = (metric) => {
+    const {max, step} = getMetricsMetaInfo(metric);
+
+    this.setState((state) => {
+      const count = state[metric] + step;
+      return {
+        ...state,
+        [metric]: count > max ? max : count
+      };
+    })
+  };
+
+  decrement = (metric) => {
+
+    this.setState((state) => {
+      const count = state[metric] - getMetricsMetaInfo(metric).step;
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count
+      };
+    })
+  };
+
+  slide = (metric, newValue) => {
+    this.setState((state) => {
+      return {
+        ...state,
+        [metric]: newValue
+      }
+    })
+  };
+
   render() {
     return (
       <View>
