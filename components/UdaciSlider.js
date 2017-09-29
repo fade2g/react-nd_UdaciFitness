8dc488493491d786cc2a1056436e032685be2d52
fade2g <holger.heymanns@gmail.com>
2017-09-29 21:12:23 +0200

    feature: Style UdaciSlider, DateHeader and TextButton

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import {View, Slider, Text} from 'react-native';
+import {View, Slider, Text, StyleSheet} from 'react-native';
+import {gray} from "../utils/colors";
 
 
 export default UdaciSlider = ({max, unit, step, value, onChange}) => {
   return (
-    <View>
+    <View style={styles.row}>
       <Slider
+        style={{flex: 1}}
         maximumValue={max}
         minmumValue={0}
         step={step}
         value={value}
         onValueChange={onChange}
       />
-      <View>
-        <Text>{value}</Text>
-        <Text>{unit}</Text>
+      <View style={styles.metricCounter}>
+        <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+        <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center'
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
