2463aef4e3c036d6d7daa761f8854ffec4e47f92
fade2g <holger.heymanns@gmail.com>
2017-09-30 22:26:34 +0200

    feature: Styling and componentizing of history view

--- App.js
+++ App.js
@@ -4,13 +4,16 @@ import AddEntry from './components/AddEntry';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers'
+import History from "./components/History";
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <AddEntry/>
+          {/* <AddEntry/> */}
+          <View style={{height: 20}}/>
+          <History/>
         </View>
       </Provider>
     );
