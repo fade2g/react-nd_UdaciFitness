3fe58a061ea7cbfae7d926a80ce3862ae8b0a7f6
fade2g <holger.heymanns@gmail.com>
2017-10-01 20:58:03 +0200

    feature: Add tab navigation and make AddEntry and History available
    Navigation is done by react-navigation library

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {View, Platform} from 'react-native';
 import AddEntry from './components/AddEntry';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers'
 import History from "./components/History";
+import { TabNavigator} from 'react-navigation'
+import { FontAwesome , Ionicons} from '@expo/vector-icons';
+import {purple, white} from "./utils/colors";
+
+const Tabs = TabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+    }
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+});
 
 export default class App extends Component {
   render() {
@@ -13,7 +51,7 @@ export default class App extends Component {
         <View style={{flex: 1}}>
           {/* <AddEntry/> */}
           <View style={{height: 20}}/>
-          <History/>
+          <Tabs />
         </View>
       </Provider>
     );
